49695f5e81adfafca3156155a6193835d4508bb2
Joana <joana.barbermo@elorrieta-errekamari.com>
2024-01-11 17:33:29 +0100

    First Commit

---
+++ src/main/java/com/reto/elorchat/ElorchatApplication.java
@@ -0,0 +1,13 @@
+package com.reto.elorchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElorchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElorchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/elorchat/ElorchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.elorchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElorchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
